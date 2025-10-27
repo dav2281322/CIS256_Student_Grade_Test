a75ef2c5bd91b1b449788c4fa7feddfb166df922
dav2281322 <dav2281322@maricopa.edu>
2025-10-27 01:48:35 -0700

    Added __init__.py file

---
+++ __init__.py
@@ -0,0 +1,21 @@
+"""
+David Wilson
+Module 08 Lab Programming Assignment
+
+Part B of B
+
+File Name: __init__.py
+Additional Files: *.py, README.md
+
+Python package initialization file for the CIS256_Student_Grade_Test Git repository.
+"""
+
+# Start of program
+
+# Import statements
+import sys
+
+if __name__ == "__main__":  # Checks to confirm this is the active program
+    print("\nPython Git repository initialized successfully!\n\nThis is a test.")
+    input("\nPress ENTER to exit the program >> ")
+    sys.exit()
